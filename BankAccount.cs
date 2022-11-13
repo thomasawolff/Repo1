fbb31d158cb306fb5ee093581204134bcad29b70
Thomas Wolff <moosehead96@hotmail.com>
2022-11-12 17:09:38 -0700

    This is the first commit of this code to the Repo1 repository for the banking app.

---
+++ BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BankLibrary
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }   
+        }
+
+        private static uint accountNumberSeed = 4056875289;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Starting Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("\tDate\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"({item.Date.ToShortDateString()}\t{item.AmountForHumans}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ Transactions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace BankLibrary
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+
+        public string AmountForHumans { 
+            get
+            {
+                return ((int)Amount).ToWords();
+            }
+        
+        }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string notes)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = notes;
+        }   
+    }
+}
